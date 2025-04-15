d6dc3c2c021fb5faeac3cb723b9ff46b28400e28
Cauterization <71918928+Cauterization@users.noreply.github.com>
2025-04-15 12:54:27 +0300

    Add proof to EtheriumJS Mpt

--- benchmark/benchmarks/OppenZeppelin/benchmark.js
+++ benchmark/benchmarks/OppenZeppelin/benchmark.js
@@ -5,12 +5,16 @@ import { utf8ToBytes } from '@ethereumjs/util';
 
 export const name = "https://github.com/OpenZeppelin/merkle-tree"
 
-
-
 export function rootBuildingSimple(leaves) {
   const tree = SimpleMerkleTree.of(leaves)
   // console.log(tree.root)
   return tree
+};
+
+export function simpleMTProof (tree, elem) {
+  try {tree.getProof(elem)} catch (e) {
+    if (e != "InvalidArgumentError: Leaf is not in tree") throw e
+  }
 };
 
 export function rootBuildingStandart(leaves) {
@@ -18,3 +22,9 @@ export function rootBuildingStandart(leaves) {
   // console.log(tree.root)
   return tree
 }
+
+export function standartMpProof(tree, elem) {
+  try {tree.getProof(elem)} catch (e) {
+    if (e != "InvalidArgumentError: Leaf is not in tree") throw e
+  }
+};
--- benchmark/benchmarks/benchmark.mjs
+++ benchmark/benchmarks/benchmark.mjs
@@ -4,35 +4,22 @@ import * as EthVerkle from './EtheriumJs/Verkle/benchmark.js'
 import * as MerkleTreeJs from './MerkleTreeJs/benchmark.js'
 import { utf8ToBytes } from '@ethereumjs/util';
 
-bench();
+// rootBuildingBenches();
+proofBenches();
 
-async function bench () {
-  await rootBuildingBench
-    ("SimpleMerkleTree " + OZ.name, OZ.rootBuildingSimple);
-  await rootBuildingBench
-    ("StandardMerkleTree " + OZ.name, OZ.rootBuildingStandart, genKVLeavesCommon);
-  await rootBuildingBench
-    (EthMpt.name, EthMpt.rootBuilding);
-  await rootBuildingBench
-    (EthVerkle.name, EthVerkle.rootBuilding, genKVLeavesCommon);
-  await rootBuildingBench
-    ("sorted " + MerkleTreeJs.name, (leaves) => MerkleTreeJs.rootBuilding(true, leaves));
-  await rootBuildingBench
-    ("unsorted " + MerkleTreeJs.name, (leaves) => MerkleTreeJs.rootBuilding(false, leaves));
+async function proofBenches() {
+  // There is no exclusion proof in OZ
+  await proofBench (EthMpt.name, EthMpt.rootBuilding, EthMpt.makeProof, genKVLeavesCommon, genKVNonMembersCommon)
+  // There is no exclusion proof in MerkleTreeJs, however it can be implemented if tree is sorted
 }
 
-async function rootBuildingBench (name, buildRoot, genLeaves = genLeavesCommon) {
-  const rootBuildingLeavesNum = [1000, 10000, 100000];
-  // const rootBuildingLeavesNum = [1000];
-
-  console.log("\n\n\nroot building", name)
-  for (const numLeaves of rootBuildingLeavesNum) {
-      const leaves = genLeaves(numLeaves)
-      const start = Date.now();
-      await buildRoot(leaves);
-      const duration = Date.now() - start;
-      console.log(`${numLeaves} leaves: ${duration} ms"`)
-  }
+async function rootBuildingBenches () {
+  await rootBuildingBench ("SimpleMerkleTree " + OZ.name, OZ.rootBuildingSimple);
+  await rootBuildingBench ("StandardMerkleTree " + OZ.name, OZ.rootBuildingStandart, genKVLeavesCommon);
+  await rootBuildingBench (EthMpt.name, EthMpt.rootBuilding);
+  await rootBuildingBench (EthVerkle.name, EthVerkle.rootBuilding, genKVLeavesCommon);
+  await rootBuildingBench ("sorted " + MerkleTreeJs.name, (leaves) => MerkleTreeJs.rootBuilding(true, leaves));
+  await rootBuildingBench ("unsorted " + MerkleTreeJs.name, (leaves) => MerkleTreeJs.rootBuilding(false, leaves));
 }
 
 async function proofBench (name, buildTree, makeProof, genLeaves = genLeavesCommon, genNonMembers = genNonMembersCommon) {
@@ -43,8 +30,24 @@ async function proofBench (name, buildTree, makeProof, genLeaves = genLeavesComm
   const tree = await buildTree(leaves);
   const nonMembers = genNonMembers(leaves, proofsNum);
   const start = Date.now();
-  for (const nonMember in nonMembers) {
-    makeProof()
+  for (const nonMember of nonMembers) {
+    makeProof(tree, nonMember);
+  }
+  const duration = Date.now() - start;
+  console.log(`${duration} ms"`)
+}
+
+async function rootBuildingBench (name, buildRoot, genLeaves = genLeavesCommon) {
+  // const rootBuildingLeavesNum = [1000, 10000, 100000];
+  const rootBuildingLeavesNum = [1000];
+
+  console.log("\n\n\nroot building", name)
+  for (const numLeaves of rootBuildingLeavesNum) {
+      const leaves = genLeaves(numLeaves)
+      const start = Date.now();
+      await buildRoot(leaves);
+      const duration = Date.now() - start;
+      console.log(`${numLeaves} leaves: ${duration} ms"`)
   }
 }
 
@@ -70,14 +73,26 @@ function genKVLeavesCommon(numLeaves) {
   return leaves;
 }
 
-
-
-
 function genNonMembersCommon(leaves, numNonMembers) {
-  const nonMembers = [];
+  const nonMembers = Array.from([]);
   for (let i = 0;; i++) {
-    const value = utf8ToBytes(`non-member-${i}`).slice(0, 31);
-    if (!leaves.has(value)) nonMembers.push(value);
-    if (nonMembers.length() >= numNonMembers) return nonMembers;
+    const paddedValue = new Uint8Array(31);
+    paddedValue.set(utf8ToBytes(`non-member-${i}`).slice(0, 31));
+    if (!leaves.includes(paddedValue)) nonMembers.push(paddedValue);
+    if (nonMembers.length >= numNonMembers) return nonMembers;
   }
 }
+
+function genKVNonMembersCommon(leaves, numNonMembers) {
+  const nonMembers = Array.from([]);
+  for (let i = 0;; i++) {
+    const paddedKey = new Uint8Array(31);
+    paddedKey.set(utf8ToBytes(`non-member-${i}`).slice(0, 31));
+    const paddedValue = new Uint8Array(31);
+    paddedValue.set(utf8ToBytes(`non-member-${i}`).slice(0, 31));
+    const kv = [paddedKey, paddedValue ]
+    if (!leaves.includes(kv)) nonMembers.push(kv);
+    if (nonMembers.length >= numNonMembers) return nonMembers;
+  }
+}
+
